1af69d6a1ded84e3b11b519bbfff24f315dac667
Ilya_Prostakov <prostocowtawer@gmail.com>
2022-04-25 19:46:34 +0700

    Redone

--- Practice4/1/main.py
+++ Practice4/1/main.py
@@ -2,7 +2,6 @@ class ListNode:
     def __init__(self, other):
         self.object = other
         self.next = None
-        self.pre = None
 
 
 class List:
@@ -10,10 +9,21 @@ class List:
         self.head = None
 
     def add_in_top(self, other):
-        pass
+        if self.head is None:
+            self.head = ListNode(other)
+            return
+        buf = ListNode(other)
+        buf.next = self.head
+        self.head = buf
 
     def add_in_end(self, other):
-        pass
+        if self.head is None:
+            self.head = ListNode(other)
+            return
+        buf = self.head
+        while buf.next is not None:
+            buf = buf.next
+        buf.next = ListNode(other)
 
     def __str__(self):
         word = ""
@@ -25,38 +35,45 @@ class List:
         return word
 
 
-class Deque(List):
+class Deque:
+    def __init__(self):
+        self.list_of_pre = List()
+        self.local_list = List()
+
     def add_in_top(self, other):
-        if self.head is None:
-            self.head = ListNode(other)
-            return
-        buf = ListNode(other)
-        self.head.pre = buf
-        buf.next = self.head
-        self.head = buf
+        self.local_list.add_in_top(other)
+        self.list_of_pre.add_in_top(None)
+        if self.local_list.head.next is not None:
+            self.list_of_pre.head.next = self.local_list.head
 
     def add_in_end(self, other):
-        if self.head is None:
-            self.head = ListNode(other)
-            return
+        self.local_list.add_in_end(other)
 
-        buf = self.head
+    def __str__(self):
+        word = ""
+        buf = self.local_list.head
 
-        while buf.next is not None:
+        while buf is not None:
+            word = word + str(buf.object) + " "
             buf = buf.next
-
-        buf1 = ListNode(other)
-        buf.next = buf1
-        buf1.pre = buf
+        return word
 
 
 def main():
+    # my_list = List()
+    # my_list.add_in_top(5)
+    # my_list.add_in_top(4)
+    # my_list.add_in_top(3)
+    # my_list.add_in_top(2)
+    # my_list.add_in_top(1)
+    # my_list.add_in_top(6)
+    # print(my_list)
     deque = Deque()
     deque.add_in_top(5)
     deque.add_in_top(4)
     deque.add_in_top(3)
     deque.add_in_top(2)
-    deque.add_in_end(6)
+    deque.add_in_end(10)
     print(deque)
 
 
